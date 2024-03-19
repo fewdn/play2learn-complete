4707d1b31c8b9c70206127ce2a9ef6955c0059c0
Owen <username@example.com>
2024-03-19 14:00:24 -0700

    update game score views and urls

--- games/urls.py
+++ games/urls.py
@@ -7,9 +7,9 @@ app_name = 'games'
 urlpatterns = [
     path('', HomepageView.as_view()), # homepage route seems like it should be in pages app but VueJS games won't display
     path('anagram-hunt/', AnagramHuntView.as_view(), name='anagram-hunt'),
-    path('games/anagram-hunt-scores/', AnagramHuntScoresView.as_view(), name='anagram-hunt-scores'),
-    path('games/anagram-hunt-user-scores/', AnagramHuntUserScoresView.as_view(), name='anagram-hunt-user-scores'), # try replace with slug for uid
+    path('anagram-hunt-scores/', AnagramHuntScoresView.as_view(), name='anagram-hunt-scores'),
+    path('anagram-hunt-user-scores/', AnagramHuntUserScoresView.as_view(), name='anagram-hunt-user-scores'), # try replace with slug for uid
     path('math-facts/', MathFactsView.as_view(), name='math-facts'),
-    path('games/math-facts-scores', MathFactsScoresView.as_view(), name='math-facts-scores'),
-    path('math-facts-scores/', MathFactsUserScoresView.as_view(), name='math-facts-user-scores'),
+    path('math-facts-scores/', MathFactsScoresView.as_view(), name='math-facts-scores'),
+    path('math-facts-user-scores/', MathFactsUserScoresView.as_view(), name='math-facts-user-scores'),
 ]
